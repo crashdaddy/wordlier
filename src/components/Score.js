7b5764c35da09694ce4b870380789a7dafbe86b5
crashdaddy <kennethscoggins@mail.com>
2022-03-12 08:08:52 -0600

    getting rid of NaN score % when 0 games played

--- src/components/Score.js
+++ src/components/Score.js
@@ -6,6 +6,8 @@ class Score extends Component {
     
     render() {
      let percentWon = parseFloat((this.props.gamesWon/this.props.gamesPlayed)*100).toFixed(1);
+
+     if (isNaN(percentWon)) {percentWon=0;}
        return (
         <div style={{verticalAlign:"middle",fontSize:"20px"}}>
             Score: {this.props.score} Played: {this.props.gamesPlayed} Won: {this.props.gamesWon} -- {percentWon}% <img src={deleteIcon} alt= "Delete Scores?" style={{display:"inline-block",verticalAlign:"middle",width:"32px",height:"32px",paddingBottom:"8px"}} onClick={this.props.clearScores} />
